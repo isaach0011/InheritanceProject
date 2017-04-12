476e19390eb786dcc7c7084c3b76556967110fee
isaach0011 <ihil17523@740s-pl-121.local>
2017-04-12 11:33:07 -0600

    Added Seth and Duncan's classes.

---
+++ src/inherit/model/Hearing.java
@@ -0,0 +1,36 @@
+/*
+ * @autohr
+ */
+package inherit.model;
+
+import inherit.model.LoudNoises;
+
+public class Hearing implements LoudNoises
+{
+	public double clapVolume(Double decibleLevel)
+	{
+		decibleLevel = decibleLevel;
+		return decibleLevel;
+	}
+	
+	public double duncanClap()
+	{
+		double decibleLevel = 99999999.99;
+		return decibleLevel;
+	}
+
+	
+	public double shotgunBlast() 
+	{
+		double decibleLevel = 187.96;
+		return decibleLevel;
+	}
+
+	
+	public String carHorn() 
+	{
+		String carHorn = "Beep, Honk, Beep";
+		
+		return carHorn;
+	}
+}
---
+++ src/inherit/model/SethMorris.java
@@ -0,0 +1,30 @@
+/*
+ * @author Seth Morris
+ */
+package inherit.model;
+
+import inherit.model.LoudNoises;
+
+public class SethMorris implements LoudNoises
+{
+	public double clapVolume(Double decibleLevel)
+	{
+		decibleLevel = 999.999;
+		return decibleLevel;
+	}
+	public double shotgunBlast()
+	{
+		double power = 420.69;
+		return power;
+	}
+	public String carHorn()
+	{
+		String hornSound = "Mexican jingle tune";
+		return hornSound;
+	}
+	public double duncanClap()
+	{
+		double didEardrumsBreak = 1.0;
+		return didEardrumsBreak;
+	}
+}
